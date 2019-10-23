98f690d713405e41eca5f0227b2d17f4aea293ec
Alec Wells <alec.wells47@gmail.com>
2019-10-23 07:53:20 -0600

    Google Oauth tentatively working adn creates user in db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
+    # binding.pry
+    @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
+    session[:user_id] = @user.id
+    redirect_to '/'
   end
 
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    :name => "google",
+    # :name => "google",
     # add in more?
-    :scope => ['contacts','plus.login','plus.me','email','profile'],
+    :scope => ['plus.login','plus.me','email','profile'],
     # this line was added to get rid of OmniAuth::Strategies::OAuth2::CallbackError
     # csrf_detected, may need different appraoch
     # provider_ignores_state: true,
+    :skip_jwt => true,
     :prompt => "select_account",
     :image_aspect_ratio => "square",
     :image_size => 50,

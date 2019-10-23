3ef5600770f1dafcaecc91c0cff2e77695f11eb1
Alec Wells <alec.wells47@gmail.com>
2019-10-23 13:15:26 -0600

    Set up mail catcher and add mailer method to sessions create
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
 
   def create
-
     @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
     session[:user_id] = @user.id
     if @user.verified?
       redirect_to root_path
     else
-      
+      UserVerificationMailer.verify(@user, server_origin).deliver_now
+      redirect_to root_path
     end
   end
 
@@ -16,4 +16,8 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+private
+  def server_origin
+    request.env["HTTP_HOST"]
+  end
 end

39ec978a5c0a24879cf8b1f1dd6968c571e9a3a6
Alec Wells <alec.wells47@gmail.com>
2019-10-23 16:32:53 -0600

    Update spec helper stub_omniauth method
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    # binding.pry
-    # @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
-    # session[:user_id] = @user.id
-    # redirect_to '/'
+    @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
+    session[:user_id] = @user.id
+    redirect_to root_path
   end
 
 end

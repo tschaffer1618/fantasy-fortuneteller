82199891f07258a018342df71681700d4438e528
Alec Wells <alec.wells47@gmail.com>
2019-10-23 13:15:26 -0600

    Finish feature test for user email verification
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

---
+++ spec/features/users/user_email_verification_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'email verification' do
+  it ' user can verify account through email' do
+    stub_omniauth
+    visit root_path
+    click_link "Sign in with Google"
+
+    user = User.last
+    expect(user.verified).to eq(false)
+    expect(page).to have_content('This account has not been verified.  Please check your email.')
+
+    visit "verification/#{user.id}"
+
+    user = User.last
+
+    expect(page).to have_content("Thank you!  Your account has been activated.")
+    expect(user.verified).to eq(true)
+
+    username = 'fft'
+    expect(page).to have_conent("Please choose a username")
+    fill_in 'user[username]', with: ""
+    click_on 'Submit'
+
+    expect(page).to have_conent("No username chosen")
+
+    fill_in 'user[username]', with: username
+    click_on 'Submit'
+
+    expect(current_path).to eq root_path
+  end
+end

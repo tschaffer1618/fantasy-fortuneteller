94897e2ff5e643fc960a730122642f7b1be17a2a
Alec Wells <alec.wells47@gmail.com>
2019-10-23 13:15:26 -0600

    Move user login/register/logout to nav bar
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
+
   get '/auth/:provider/callback', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   get 'auth/failure', to: redirect('/')
 end
--- spec/features/visitors/visitor_can_register_with_google_spec.rb
+++ spec/features/visitors/visitor_can_register_with_google_spec.rb
@@ -4,12 +4,19 @@ describe 'Visitor can register' do
   scenario 'they can authorize with google oauth2' do
     stub_omniauth
     visit root_path
+
     expect(page).to have_link("Sign in with Google")
+
     click_link "Sign in with Google"
-    user = User.first
-    expect(user.email).to eq('alec@gmail.com')
-    expect(user.first_name).to eq('Alec')
+
     expect(page).to have_content("Alec Wells")
     expect(page).to have_link("Logout")
+
+    click_link 'Logout'
+
+    expect(page).to_not have_content("Alec Wells")
+    expect(page).to_not have_link("Logout")
+
+    expect(page).to have_link('Sign in with Google')
   end
 end

98f497203f7c774f9bff47bd1e9ccb24230fd7e1
Alec Wells <alec.wells47@gmail.com>
2019-10-23 13:15:26 -0600

    Finish omniauth setup and tests
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- spec/features/visitors/visitor_can_register_with_google_spec.rb
+++ spec/features/visitors/visitor_can_register_with_google_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe 'Visitor can register' do
-  it 'they can authorize with google oauth2' do
+  scenario 'they can authorize with google oauth2' do
     stub_omniauth
     visit root_path
     expect(page).to have_link("Sign in with Google")
     click_link "Sign in with Google"
-    expect(page).to have_content("Alec Wells")
-    expect(page).to have_link("Logout")
+    user = User.first
+    expect(user.email).to eq('alec@gmail.com')
+    expect(user.first_name).to eq('Alec')
+    # expect(page).to have_content("Alec Wells")
+    # expect(page).to have_link("Logout")
   end
 end

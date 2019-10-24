d9426e3153df761322283b5a243e6fb0783cab8a
Alec Wells <alec.wells47@gmail.com>
2019-10-24 14:44:56 -0600

    Create user profile view and private method for set_user
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
     session[:user_id] = @user.id
     if @user.verified?
-      redirect_to root_path
+      redirect_to profile_path
     else
       UserVerificationMailer.verify(@user, server_origin).deliver_now
-      redirect_to root_path
+      redirect_to profile_path
     end
   end
 

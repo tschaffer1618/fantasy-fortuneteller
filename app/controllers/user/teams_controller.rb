501231cfc6f7e6d5874797195969ba49f3997085
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-24 15:02:57 -0600

    Create new user team functionality and tests

--- app/controllers/user/teams_controller.rb
+++ app/controllers/user/teams_controller.rb
@@ -7,5 +7,18 @@ class User::TeamsController < ApplicationController
   end
 
   def new
+    @team = Team.new
+  end
+
+  def create
+    team = current_user.teams.create(team_params)
+    flash[:success] = "New team created!" if team.save
+    redirect_to user_teams_path
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
   end
 end
--- spec/features/users/user_team_index_page_spec.rb
+++ spec/features/users/user_team_index_page_spec.rb
@@ -15,8 +15,24 @@ describe "A logged in user" do
       expect(page).to have_link("What The Flacco")
       click_link("The Brady Bunch")
     end
-    
+
     expect(current_path).to eq user_team_path(@team_2)
   end
 
+  scenario "can add a new team" do
+    visit user_teams_path
+
+    click_link("New Team")
+
+    expect(current_path).to eq new_user_team_path
+
+    fill_in "Name", with: "Test Team"
+    click_button("Create Team")
+
+    expect(current_path).to eq user_teams_path
+
+    within('.table') do
+      expect(page).to have_link("Test Team")
+    end
+  end
 end

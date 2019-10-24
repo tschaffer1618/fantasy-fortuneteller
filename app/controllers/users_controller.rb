67d18dc0ce512feb4dcc8c9ae5a26970929f6f09
Alec Wells <alec.wells47@gmail.com>
2019-10-24 15:38:43 -0600

    Add verify action to user_controller to update user verified
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :verify]
 
   def show
+  end
+
+  def verify
+    user = User.find(params[:user_id])
+    user.update_attribute(:verified, true)
+    redirect_to profile_path
   end
 
 private

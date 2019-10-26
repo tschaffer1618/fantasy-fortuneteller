e3a8170f73ed1876cdbb63088ea57d6e91ba2af7
Alec Wells <alec.wells47@gmail.com>
2019-10-26 15:19:12 -0600

    Write test for select player by position
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

---
+++ spec/features/players/select_position_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Players' do
+  before(:each) do
+    @user = create(:user, user_name: "Jason Bourne")
+    @tom_brady = Player.create!(display_name: 'Tom Brady', position: 'QB')
+    @ryan_fitzpatrick = Player.create!(display_name: 'Ryan Fitzpatrick', position: 'QB')
+    @christian = Player.create!(display_name: 'Christian McCaffrey', position: 'RB')
+    @deandre = Player.create!(display_name: 'DeAndre Hopkins', position: 'WR')
+    @ravens = Player.create!(display_name: 'Balitmore Ravens', position: 'DEF')
+    @greg = Player.create!(display_name: 'Greg Zuerlein', position: 'K')
+    @george = Player.create!(display_name: 'George Kittle', position: 'TE')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  it ' user can see players for specific position' do
+    visit players_path
+
+    click_on 'Position'
+    within '.position-dropdown' do
+      expect(page).to have_content("All")
+      expect(page).to have_content("Quarter Back")
+      expect(page).to have_content("Wide Receiver")
+      expect(page).to have_content("Running Back")
+      expect(page).to have_content("Kicker")
+      expect(page).to have_content("Tight End")
+      expect(page).to have_content("Defense")
+      click_on 'Quarter Back'
+    end
+
+    expect(page).to have_content(@tom_brady.display_name)
+    expect(page).to have_content(@ryan_fitzpatrick.display_name)
+    expect(page).to_not have_content(@christian.display_name)
+  end
+end

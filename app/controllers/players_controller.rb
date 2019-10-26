f731f868ffa682d64a510313449e623a6494714f
Alec Wells <alec.wells47@gmail.com>
2019-10-26 15:19:12 -0600

    Add functionality to sort players in player#index
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,10 @@
 class PlayersController < ApplicationController
   before_action :require_user
+  helper_method :sort_column, :sort_direction
 
   def index
-    @players = Player.search(params[:search])
+    players = Player.search(params[:search])
+    @players = players.order(sort_column + " " + sort_direction)
     # @teams = current_user.teams
     # @team_player = @teams.each...
   end
@@ -12,5 +14,13 @@ class PlayersController < ApplicationController
   end
 
 private
+
+  def sort_column
+    Player.column_names.include?(params[:sort]) ? params[:sort] : "display_name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 
 end
--- app/models/team_player.rb
+++ app/models/team_player.rb
@@ -2,7 +2,7 @@ class TeamPlayer < ApplicationRecord
   belongs_to :team
   belongs_to :player
 
-  def self.does_not_exist?(team_player_params)
-    TeamPlayer.where(team_player_params).empty?
-  end
+  # def self.does_not_exist?(team_player_params)
+  #   TeamPlayer.where(team_player_params).empty?
+  # end
 end

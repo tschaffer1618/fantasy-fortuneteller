70df5993f444470e5390a0e42c58a14dc45f7746
Alec Wells <alec.wells47@gmail.com>
2019-10-26 15:19:12 -0600

    Update test coverage for benched/unbenched

--- spec/features/teams/team_player_benched_spec.rb
+++ spec/features/teams/team_player_benched_spec.rb
@@ -5,8 +5,8 @@ describe "A logged in user on a team show page" do
     @user = create(:user, user_name: "Jason Bourne")
     @team_1 = @user.teams.create(name: "What The Flacco")
     @team_2 = @user.teams.create(name: "spooooky")
-    @player_1 = create(:player, display_name: "Frank Gore", photo_url: 'http://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/BRA371156.png')
-    @player_2 = create(:player, display_name: "Aaron Jones", photo_url: 'http://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/BRA371156.png')
+    @player_1 = create(:player, display_name: "Frank Gore", current_projection: 4, photo_url: 'http://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/BRA371156.png')
+    @player_2 = create(:player, display_name: "Aaron Jones", current_projection: 2, photo_url: 'http://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/BRA371156.png')
     @player_3 = create(:player, display_name: "Tom Brady", photo_url: 'http://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/BRA371156.png')
     @team_player_1 = @team_1.team_players.create(player: @player_1)
     @team_player_2 = @team_1.team_players.create(player: @player_2)
@@ -16,7 +16,7 @@ describe "A logged in user on a team show page" do
 
   scenario "can change player status to benched or unbenched" do
     visit user_team_path(@team_1)
-    
+
     within "#player-section-#{@player_1.id}" do
       within ".benched" do
         expect(page).to have_content("Put me in")
@@ -36,5 +36,27 @@ describe "A logged in user on a team show page" do
 
     expect(current_path).to eq(user_team_path(@team_1))
     # expect(@team_player_1.benched).to eq(true)
+  end
+
+  it "shows total_projected_score" do
+    visit user_team_path(@team_1)
+
+    expect(page).to have_content("Total Projected Score: 0.0")
+
+    within "#player-section-#{@player_1.id}" dox
+      within ".benched" do
+        click_on "Put me in!"
+      end
+    end
+
+    expect(page).to have_content("Total Projected Score: 4.0")
+
+    within "#player-section-#{@player_2.id}" do
+      within ".benched" do
+        click_on "Put me in!"
+      end
+    end
+
+    expect(page).to have_content("Total Projected Score: 6.0")
   end
 end

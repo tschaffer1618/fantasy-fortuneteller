c608c158aa672912f740ffaf0548d60e3b0e6e04
Alec Wells <alec.wells47@gmail.com>
2019-10-26 15:19:12 -0600

    Remove unecessary line from create action in user/teams_controller
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/user/teams_controller.rb
+++ app/controllers/user/teams_controller.rb
@@ -32,9 +32,9 @@ class User::TeamsController < ApplicationController
 
   def update
     team = current_user.teams.find(params[:id])
-    team.update(team_params)
-    # update autmatically saves to db, so "if team.save" probably unecessary
-    if team.save
+    if team.update(team_params)
+    # i think update autmatically saves to db, so "if team.save" probably unecessary
+    # if team.save
       flash[:success] = "Team name updated!"
       redirect_to user_teams_path
     else

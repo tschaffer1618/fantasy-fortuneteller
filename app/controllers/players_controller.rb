65d6c6b44faa1cb0ee89a141fae67e8da9806660
Alec Wells <alec.wells47@gmail.com>
2019-10-26 15:19:12 -0600

    Change default so player index sorts by projection
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -19,10 +19,10 @@ class PlayersController < ApplicationController
   private
 
   def sort_column
-    Player.column_names.include?(params[:sort]) ? params[:sort] : "display_name"
+    Player.column_names.include?(params[:sort]) ? params[:sort] : "current_projection"
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end

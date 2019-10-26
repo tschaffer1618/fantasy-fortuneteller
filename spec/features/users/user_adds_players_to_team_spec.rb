739fc60d810dbcb296b5e67b541b7fc77f174a14
Alec Wells <alec.wells47@gmail.com>
2019-10-26 15:19:12 -0600

    Write user add player spec
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>

---
+++ spec/features/users/user_adds_players_to_team_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "User can add player to team" do
+  before(:each) do
+    @user = create(:user, user_name: "Jason Bourne")
+    @team_1 = @user.teams.create(name: "What The Flacco")
+    @team_2 = @user.teams.create(name: "The Brady Bunch")
+    @ryan_fitzpatrick = Player.create!(display_name: 'Ryan Fitzpatrick')
+    @joe_flacco = Player.create!(display_name: 'Joe Flacco')
+    @chad_henne = Player.create!(display_name: 'Chad Henne')
+    @derek_anderson = Player.create!(display_name: 'Derek Anderson')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  scenario "can visit team index and select player to add to team" do
+    visit players_path
+
+    within "#player-#{@ryan_fitzpatrick.id}" do
+      expect(page).to have_content("Add to team")
+      click_on("Add to team")
+      expect(page).to have_link("#{@team_1.name}")
+      expect(page).to have_link("#{@team_2.name}")
+
+      click_link "#{@team_1.name}"
+    end
+
+    expect(current_path).to eq(players_path)
+
+    # within "#player-#{@ryan_fitzpatrick.id}" do
+    #   expect(page).to_not have_content("Add to team")
+    # end
+
+    visit user_team_path(@team_1)
+
+    expect(page).to have_content(@ryan_fitzpatrick.display_name)
+    expect(page).to have_content(@ryan_fitzpatrick.position)
+  end
+end

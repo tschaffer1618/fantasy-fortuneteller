03441f7d12982e6c7e0055be61c962432a918b50
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-28 11:44:34 -0600

    Test dependent destroy for teams and team_players

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -9,4 +9,15 @@ describe Team, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
   end
+
+  describe "dependencies" do
+    it "destroys team_players when a team is destroyed" do
+      user = create(:user, user_name: "Test")
+      player = create(:player)
+      team = user.teams.create(name: "Test team")
+      team_player = team.team_players.create(player: player)
+
+      expect { team.destroy }.to change { TeamPlayer.count }.by(-1)
+    end
+  end
 end

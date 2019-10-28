780d1ab8ee53e71de10d4035515f3d979a58eeca
tschaffer1618 <tpschaffer@gmail.com>
2019-10-28 11:18:11 -0600

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :user
-  has_many :team_players
+  has_many :team_players, dependent: :destroy
   has_many :players, through: :team_players
 
   validates_presence_of :name

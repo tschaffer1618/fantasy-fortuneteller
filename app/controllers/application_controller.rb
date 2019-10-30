e9a6f52f578c014293c0a4d326a9b06632ea56be
Fenton Taylor <fentontaylor@gmail.com>
2019-10-29 20:06:00 -0600

    Require verified user to see full profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :verified_user?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def verified_user?
+    current_user && current_user.verified
   end
 
   def require_user
@@ -12,5 +17,9 @@ class ApplicationController < ActionController::Base
       flash[:error] = "Please sign in to see this content."
       redirect_to root_path
     end
+  end
+
+  def require_verified_user
+    
   end
 end
---
+++ spec/features/users/users_must_verify_email_before_accessing_content_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'As a new user' do
+  context 'I have not verified my email yet' do
+    before :each do
+      @user = create(:user, verified: false)
+      allow_any_instance_of(ApplicationController)
+        .to receive(:current_user).and_return(@user)
+    end
+
+    scenario 'I can access my profile, but it tells me to check my email' do
+      visit profile_path
+
+      expect(current_path).to eq(profile_path)
+      expect(page).to have_content(@user.email)
+      expect(page).to_not have_link('Edit Profile')
+      expect(page).to_not have_css('.teams')
+    end
+
+    scenario 'I cannot visit "My Teams"' do
+      
+    end
+  end
+end

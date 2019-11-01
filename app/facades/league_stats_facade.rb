9b8103feb416d0c4f14d3281590985bbaf66c22d
Fenton Taylor <fentontaylor@gmail.com>
2019-10-31 19:25:35 -0600

    Complete league stats facade for getting top5

--- app/facades/league_stats_facade.rb
+++ app/facades/league_stats_facade.rb
@@ -1,25 +1,31 @@
 class LeagueStatsFacade
   def quarterbacks
-
+    top_5('QB')
   end
 
   def running_backs
-
+    top_5('RB')
   end
 
   def wide_receivers
-
+    top_5('WR')
   end
 
   def tight_ends
-
+    top_5('TE')
   end
 
   def kickers
-
+    top_5('K')
   end
 
   def defenses
+    top_5('DEF')
+  end
 
+  private
+
+  def top_5(pos)
+    Player.current_top_5(pos)
   end
 end

ea79bf4c04a484e91d3711d2ec276d81fc27942c
Fenton Taylor <fentontaylor@gmail.com>
2019-10-31 18:16:35 -0600

    Create facade shell

---
+++ app/facades/league_stats_facade.rb
@@ -0,0 +1,25 @@
+class LeagueStatsFacade
+  def quarterbacks
+
+  end
+
+  def running_backs
+
+  end
+
+  def wide_receivers
+
+  end
+
+  def tight_ends
+
+  end
+
+  def kickers
+
+  end
+
+  def defenses
+
+  end
+end

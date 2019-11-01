129e3bb2f5f6afa2a6fdf527ed4dc419eb460e01
Fenton Taylor <fentontaylor@gmail.com>
2019-10-31 21:04:11 -0600

    Update test to match 6 players

--- spec/features/users/user_views_top5_on_league_stats_spec.rb
+++ spec/features/users/user_views_top5_on_league_stats_spec.rb
@@ -30,7 +30,7 @@ context 'As a logged in user' do
       @wr4 = create(:player, position: 'WR', current_projection: 20)
       @wr5 = create(:player, position: 'WR', current_projection: 25)
       @wr6 = create(:player, position: 'WR', current_projection: 30)
-      @wr7 = create(:player, position: 'RB', current_projection: 30.5)
+      @wr7 = create(:player, position: 'WR', current_projection: 30.5)
 
 
       @te1 = create(:player, position: 'TE', current_projection: 5)
@@ -39,7 +39,7 @@ context 'As a logged in user' do
       @te4 = create(:player, position: 'TE', current_projection: 20)
       @te5 = create(:player, position: 'TE', current_projection: 25)
       @te6 = create(:player, position: 'TE', current_projection: 30)
-      @te7 = create(:player, position: 'RB', current_projection: 30.5)
+      @te7 = create(:player, position: 'TE', current_projection: 30.5)
 
       @k1 = create(:player, position: 'K', current_projection: 5)
       @k2 = create(:player, position: 'K', current_projection: 10)
@@ -47,7 +47,7 @@ context 'As a logged in user' do
       @k4 = create(:player, position: 'K', current_projection: 20)
       @k5 = create(:player, position: 'K', current_projection: 25)
       @k6 = create(:player, position: 'K', current_projection: 30)
-      @k7 = create(:player, position: 'RB', current_projection: 30.5)
+      @k7 = create(:player, position: 'K', current_projection: 30.5)
 
 
       @def1 = create(:player, position: 'DEF', current_projection: 5)
@@ -56,7 +56,7 @@ context 'As a logged in user' do
       @def4 = create(:player, position: 'DEF', current_projection: 20)
       @def5 = create(:player, position: 'DEF', current_projection: 25)
       @def6 = create(:player, position: 'DEF', current_projection: 30)
-      @def7 = create(:player, position: 'RB', current_projection: 30.5)
+      @def7 = create(:player, position: 'DEF', current_projection: 30.5)
     end
 
     scenario 'I see a section for top 6 projected performances for each position' do
@@ -68,23 +68,23 @@ context 'As a logged in user' do
         expect(page).to have_content(@qb7.display_name)
         expect(page).to have_content("(#{@qb7.team})")
         expect(page).to have_content(@qb7.current_projection)
-        @qb7.display_name.should appear_before(@qb6.display_name)
-        @qb6.display_name.should appear_before(@qb5.display_name)
-        @qb5.display_name.should appear_before(@qb4.display_name)
-        @qb4.display_name.should appear_before(@qb3.display_name)
-        @qb3.display_name.should appear_before(@qb2.display_name)
+        expect(@qb7.display_name).to appear_before(@qb6.display_name)
+        expect(@qb6.display_name).to appear_before(@qb5.display_name)
+        expect(@qb5.display_name).to appear_before(@qb4.display_name)
+        expect(@qb4.display_name).to appear_before(@qb3.display_name)
+        expect(@qb3.display_name).to appear_before(@qb2.display_name)
         expect(page).to_not have_content(@qb1.display_name)
       end
 
       within ".top-6-running-backs" do
-        expect(page).to have_content(@rb6.display_name)
-        expect(page).to have_content("(#{@rb6.team})")
-        expect(page).to have_content(@rb6.current_projection)
-        @rb7.display_name.should appear_before(@rb6.display_name)
-        @rb6.display_name.should appear_before(@rb5.display_name)
-        @rb5.display_name.should appear_before(@rb4.display_name)
-        @rb4.display_name.should appear_before(@rb3.display_name)
-        @rb3.display_name.should appear_before(@rb2.display_name)
+        expect(page).to have_content(@rb7.display_name)
+        expect(page).to have_content("(#{@rb7.team})")
+        expect(page).to have_content(@rb7.current_projection)
+        expect(@rb7.display_name).to appear_before(@rb6.display_name)
+        expect(@rb6.display_name).to appear_before(@rb5.display_name)
+        expect(@rb5.display_name).to appear_before(@rb4.display_name)
+        expect(@rb4.display_name).to appear_before(@rb3.display_name)
+        expect(@rb3.display_name).to appear_before(@rb2.display_name)
         expect(page).to_not have_content(@rb1.display_name)
       end
 
@@ -92,11 +92,11 @@ context 'As a logged in user' do
         expect(page).to have_content(@wr6.display_name)
         expect(page).to have_content("(#{@wr6.team})")
         expect(page).to have_content(@wr6.current_projection)
-        @wr7.display_name.should appear_before(@wr6.display_name)
-        @wr6.display_name.should appear_before(@wr5.display_name)
-        @wr5.display_name.should appear_before(@wr4.display_name)
-        @wr4.display_name.should appear_before(@wr3.display_name)
-        @wr3.display_name.should appear_before(@wr2.display_name)
+        expect(@wr7.display_name).to appear_before(@wr6.display_name)
+        expect(@wr6.display_name).to appear_before(@wr5.display_name)
+        expect(@wr5.display_name).to appear_before(@wr4.display_name)
+        expect(@wr4.display_name).to appear_before(@wr3.display_name)
+        expect(@wr3.display_name).to appear_before(@wr2.display_name)
         expect(page).to_not have_content(@wr1.display_name)
       end
 
@@ -104,11 +104,11 @@ context 'As a logged in user' do
         expect(page).to have_content(@te6.display_name)
         expect(page).to have_content("(#{@te6.team})")
         expect(page).to have_content(@te6.current_projection)
-        @te7.display_name.should appear_before(@te6.display_name)
-        @te6.display_name.should appear_before(@te5.display_name)
-        @te5.display_name.should appear_before(@te4.display_name)
-        @te4.display_name.should appear_before(@te3.display_name)
-        @te3.display_name.should appear_before(@te2.display_name)
+        expect(@te7.display_name).to appear_before(@te6.display_name)
+        expect(@te6.display_name).to appear_before(@te5.display_name)
+        expect(@te5.display_name).to appear_before(@te4.display_name)
+        expect(@te4.display_name).to appear_before(@te3.display_name)
+        expect(@te3.display_name).to appear_before(@te2.display_name)
         expect(page).to_not have_content(@te1.display_name)
       end
 
@@ -116,11 +116,11 @@ context 'As a logged in user' do
         expect(page).to have_content(@k6.display_name)
         expect(page).to have_content("(#{@k6.team})")
         expect(page).to have_content(@k6.current_projection)
-        @k7.display_name.should appear_before(@k6.display_name)
-        @k6.display_name.should appear_before(@k5.display_name)
-        @k5.display_name.should appear_before(@k4.display_name)
-        @k4.display_name.should appear_before(@k3.display_name)
-        @k3.display_name.should appear_before(@k2.display_name)
+        expect(@k7.display_name).to appear_before(@k6.display_name)
+        expect(@k6.display_name).to appear_before(@k5.display_name)
+        expect(@k5.display_name).to appear_before(@k4.display_name)
+        expect(@k4.display_name).to appear_before(@k3.display_name)
+        expect(@k3.display_name).to appear_before(@k2.display_name)
         expect(page).to_not have_content(@k1.display_name)
       end
 
@@ -128,11 +128,11 @@ context 'As a logged in user' do
         expect(page).to have_content(@def6.display_name)
         expect(page).to have_content("(#{@def6.team})")
         expect(page).to have_content(@def6.current_projection)
-        @def7.display_name.should appear_before(@def6.display_name)
-        @def6.display_name.should appear_before(@def5.display_name)
-        @def5.display_name.should appear_before(@def4.display_name)
-        @def4.display_name.should appear_before(@def3.display_name)
-        @def3.display_name.should appear_before(@def2.display_name)
+        expect(@def7.display_name).to appear_before(@def6.display_name)
+        expect(@def6.display_name).to appear_before(@def5.display_name)
+        expect(@def5.display_name).to appear_before(@def4.display_name)
+        expect(@def4.display_name).to appear_before(@def3.display_name)
+        expect(@def3.display_name).to appear_before(@def2.display_name)
         expect(page).to_not have_content(@def1.display_name)
       end
     end
